75e45485269420f2c02029aa26a3b730e87edd58
walter_wu090 <walter.wu090@gmail.com>
2017-06-20 20:43:50 -0400

    command line argparse

--- image_download.py
+++ image_download.py
@@ -3,13 +3,14 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import os
+import argparse
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686)'
                          ' AppleWebKit/537.17 (KHTML, like Gecko)'
                          ' Chrome/24.0.1312.27 Safari/537.17'}
 
 
-def search(search_term, number=1, size=None):
+def search(search_term, number=15, size=None):
     """Search for images from Google Images
 
     Args:
@@ -24,10 +25,11 @@ def search(search_term, number=1, size=None):
     size_desig = {'large': 'l', 'medium': 'm', 'icon': 'i'}
 
     param = 'tbm=isch&q='
+    file_format = '&tbs=ift:jpg'
     root = 'https://www.google.com/search?'
 
     joined_search_term = '+'.join(search_term.split(' '))
-    url = root + param + joined_search_term
+    url = root + param + joined_search_term + file_format
 
     if size is not None:
         url = root + '&tbs=isz:' + size_desig[size] + '&tbm=isch&q=' + joined_search_term
@@ -64,6 +66,25 @@ def download(links, destination='images'):
         os.mkdir(destination)
 
     for img_link in links:
+        img_name = img_link.split('/')[-1]
         req = requests.get(img_link, stream=True, headers=headers)
-        with open(os.path.join(destination, img_link.split('/')[-1]), 'wb') as f:
+        with open(os.path.join(destination, img_name), 'wb') as f:
             f.write(req.content)
+        print('Downloaded image {}'.format(img_name))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--search-for', dest='search', type=str,
+                        help='Search term for google image search')
+    parser.add_argument('--number', dest='number', type=int, default=15,
+                        help='Number of search results to return')
+    parser.add_argument('--size', dest='size', type=str, default=None,
+                        help='Size of image results')
+    parser.add_argument('--destination', dest='destination', type=str, default='images',
+                        help='Destination to store the searched results')
+
+    args = parser.parse_args()
+
+    links = search(search_term=args.search, number=args.number, size=args.size)
+    download(links, destination=args.destination)
